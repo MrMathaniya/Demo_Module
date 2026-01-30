e66ef70301e2dc3566865b75f649eb34187feb23
Vansh Mathaniya <108232361+MrMathaniya@users.noreply.github.com>
2026-01-30 21:07:49 +0530

    Add files via upload

---
+++ formulas.py
@@ -0,0 +1,30 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+def square(a):
+    return a**2
+
+def cube(a):
+    return a**3
+
+def power(a,n):
+    return a**n
+
+def area_of_circle(a):
+    return 3.14*a*a
+
+def area_of_rectangle(a,b):
+    return a*b
+
+def area_of_square(a):
+    return a*a
+
